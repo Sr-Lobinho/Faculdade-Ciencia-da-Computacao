85d958eec1d7e850c059258eeca80bcfe61bff30
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 16:42:20 -0300

    Create Simulado3.cpp

---
+++ AEDs1/Lista01/Extras/Simulado3.cpp
@@ -0,0 +1,44 @@
+int checaAlgo(int a, int b, int c, int d, int e, int f){
+	int soma = a + b + c + d + e + f;  
+	if (a == b and a == c){
+		cout << "Os 3 primeiros valores sao iguais" << endl;
+		return 15;
+	}
+	else if(d == e and d == f){
+		cout << "Os 3 ultimos valores sao iguais" << endl;
+		return 15;
+	}
+	
+	else if (soma % f == 0){
+		cout << "\033[41m ALERTA! \033[m" << endl;
+	}
+	return soma;
+}
+void imprime(float i){
+	cout << "Informe o valor " << i << ": ";
+}
+
+int main(){
+	int a,b,c,d,e,f, valor= 0.0;
+	imprime(1);
+	cin >> a;
+	imprime(2);
+	cin >> b;
+	imprime(3);
+	cin >> c;
+	imprime(4);
+	cin >> d;
+	imprime(5);
+	cin >> e;
+	imprime(6);
+	cin >> f;
+	
+	valor = checaAlgo(a,b,c,d,e,f);
+	if (valor == 15){
+		cout << "Otimo" << endl;
+	}
+	else{
+		cout << "Excelente" << endl;
+	}
+	return 0;
+}
