c00b6c3cac64309675517ac97acc0509400f9f13
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 16:01:32 -0300

    Create Ex13.cpp

---
+++ AEDs1/Lista01/Ex13.cpp
@@ -0,0 +1,16 @@
+int main(int argc, char** argv) {
+	int h, m, s, resto = 0;
+	
+	cout << "Informe a duracao em segundos: ";
+	cin >> s;
+	
+	h = s/3600;
+	resto = s%3600;
+	m = resto/60;
+	resto %= 60;
+	
+	cout << "O evento durou " << h << " horas, " << m << " minutos e " << resto << " segundos. (" << s << ")";
+	
+	
+	return 0;
+}
