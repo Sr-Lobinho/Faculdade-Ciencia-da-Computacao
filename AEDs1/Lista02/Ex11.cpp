b8cb10455d59db7ba2e77a02b0dd8b3bd8e0ac6d
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 17:34:59 -0300

    Create Ex11.cpp

---
+++ AEDs1/Lista02/Ex11.cpp
@@ -0,0 +1,48 @@
+int validaIdade(){
+	int i = 0;
+	do{
+		cout << "Informe a sua idade: ";
+		cin >> i;
+		if (i < 0 or i > 150){
+			cout << "\033[31mIdade Invalida!\033[m" << endl;
+		}
+	}while(i < 0 or i > 150);
+	return i;
+}
+float validaSalario(){
+	float s = 0.0;
+	do{
+		cout << "Informe seu salario: ";
+		cin >> s;
+		if (s < 0){
+			cout << "\033[31mSalario Invalido!\033[m" << endl;
+		}
+	}while (s < 0);
+	return s;
+}
+char validaEC(){
+	char ec = ' ';
+	do{
+		cout << "s -   solteiro" << endl;
+		cout << "c -   casado" << endl;
+		cout << "v -   viuvo" << endl;
+		cout << "d -   divorciado" << endl << endl;
+		
+		cout << "Informe seu Estado civil(minusculo): ";
+		cin >> ec;
+		if (ec != 's' and ec != 'c' and ec != 'v' and ec != 's'){
+			cout << "\033[31mEstado Civil Invalido!\033[m" << endl;
+		}
+		
+	}while(ec != 's' and ec != 'c' and ec != 'v' and ec != 's');
+	
+	return ec;
+}
+
+int main(){
+	validaIdade();
+	validaSalario();
+	validaEC();
+	return 0;
+
+}
