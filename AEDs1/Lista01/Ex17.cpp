ad4738f6ae577189cac98de226d938f2c9378001
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 15:36:44 -0300

    Create Ex17.cpp

---
+++ AEDs1/Lista01/Ex17.cpp
@@ -0,0 +1,18 @@
+void polaridade(float n){
+	if (n < 0){
+		cout << "O numero e negativo.";
+	}
+	else if (n > 0){
+		cout << "O numero e positivo.";
+	}
+	else{
+		cout << "O numero e neutro.";
+	}
+}
+int main(int argc, char** argv) {
+	float n = 0;
+	cout << "Informe um numero: ";
+	cin >> n;
+	polaridade(n);
+	
+	return 0;
