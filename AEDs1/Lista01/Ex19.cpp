63c59133deabeeab99c3485ce582a4116b8a52f0
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 15:38:52 -0300

    Create Ex19.cpp

---
+++ AEDs1/Lista01/Ex19.cpp
@@ -0,0 +1,18 @@
+void carteiraMotorista(int i){
+	if (i >= 18){
+		cout << "Voce esta apto para tirar a carteira de motorista. ";
+	}
+	else{
+		cout << "Voce nao esta apto para tirar a carteira de motorista, ainda faltam " << 18 - i << " anos.";
+	}
+	
+}
+
+int main(int argc, char** argv) {
+	int idade = 0;
+	cout << "Informe sua idade em anos: ";
+	cin >> idade;
+	carteiraMotorista(idade);
+	
+	return 0;
+}
