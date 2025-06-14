94a660b1c105c15417a3663d8db407dbfa93ef9b
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 15:37:13 -0300

    Create Ex18.cpp

---
+++ AEDs1/Lista01/Ex18.cpp
@@ -0,0 +1,24 @@
+int verificarNota(int index){
+	float nota = 0;
+	cout << "Digite a nota " << index << ": ";
+	cin >> nota;
+	if (nota > 60){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+int main(int argc, char** argv) {
+	int count = 0;
+	count +=  verificarNota(1);
+	count +=  verificarNota(2);
+	count +=  verificarNota(3);
+	count +=  verificarNota(4);
+	count +=  verificarNota(5);
+	
+	cout << "Voce teve " << count << " notas acima de 60.";
+	
+	return 0;
+}
