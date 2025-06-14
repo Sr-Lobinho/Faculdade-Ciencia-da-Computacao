58102173774f111e2a1fb8de9d16cf69805db260
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 15:47:48 -0300

    Create Ex31.cpp

---
+++ AEDs1/Lista01/Ex31.cpp
@@ -0,0 +1,28 @@
+void classificaIdade(int i){
+	if (i < 5){
+		cout << "Voce nao possui idade para natacao.";
+	}
+	else if (i <= 7){
+		cout << "Voce esta na categoria Infantil A.";
+	}
+	else if (i <= 10){
+		cout << "Voce esta na categoria Infantil B.";
+	}
+	else if (i <= 13){
+		cout << "Voce esta na categoria Juvenil A.";
+	}
+	else if (i <= 17){
+		cout << "Voce esta na categoria Juvenil B.";
+	}
+	else if (i >= 18){
+		cout << "Voce esta na categoria Adulto.";
+	}
+}
+int main(int argc, char** argv) {
+	int idade = 0;
+	cout << "Informe sua idade: ";
+	cin >> idade;
+	classificaIdade(idade);
+	
+	return 0;
+}
