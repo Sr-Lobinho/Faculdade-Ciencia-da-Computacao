59283ce88153dcc79f5e57788ee149aa2ddf37f3
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 16:37:58 -0300

    Create Bonus2.cpp

---
+++ AEDs1/Lista01/Extras/Bonus2.cpp
@@ -0,0 +1,24 @@
+float calculaImposto(float s){
+	float total = 0;
+	if (s < 1500){
+		return total;
+	}
+	else if (s < 3000){
+		total += s * 0.12 ;
+	}
+	else if (s < 4000){
+		total +=  s * 0.2;
+	}
+	else{
+		total += s * 0.27;
+	}
+	return total;
+}
+int main(int argc, char** argv) {
+	float salario = 0.0;
+	cout << "Informe o seu salario: ";
+	cin >> salario;
+	cout << "O valor do imposto sera " << calculaImposto(salario);
+	
+	return 0;
+}
