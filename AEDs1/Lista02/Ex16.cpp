ece833f317189f8a9dd095bff00f3b2f1bf7e944
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 17:38:27 -0300

    Create Ex16.cpp

---
+++ AEDs1/Lista02/Ex16.cpp
@@ -0,0 +1,19 @@
+void calculaQuantos(){
+	int imp = 0, par = 0, n = 0;
+	
+	for (int i = 0; i < 10; i++){
+		cout << "Digite um numero: ";
+		cin >> n;
+		if (n%2 == 0){
+			par += 1;
+		}
+		else{
+			imp += 1;
+		}
+	}
+	cout << "Dos 10 valores " << par << " foram pares e " << imp << " foram impares." << endl;
+}
+int main(){
+	calculaQuantos();
+	return 0;
+}
