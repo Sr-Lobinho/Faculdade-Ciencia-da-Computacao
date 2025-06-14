074d354145c15ba3fd09ef9766bd7f50c66cf69c
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 15:47:07 -0300

    Create Ex30.cpp

---
+++ AEDs1/Lista01/Ex30.cpp
@@ -0,0 +1,23 @@
+bool verificaMultiplo(int a, int b){
+	if (a % b == 0 or b % a == 0){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+int main(int argc, char** argv) {
+	int n1, n2 = 0;
+	cout << "Informe 2 valores: " << endl;
+	cin >> n1;
+	cin >> n2;
+	
+	if (verificaMultiplo(n1,n2)){
+		cout << "Os 2 valores sao multiplos!";
+	}
+	else{
+		cout << "Os 2 valores nao sao multiplos";
+	}
+	return 0;
+}
