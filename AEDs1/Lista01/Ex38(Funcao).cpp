d146eb25c53599764fdf6ea096004130765a7a74
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 16:27:54 -0300

    Create Ex38(Funcao).cpp

---
+++ AEDs1/Lista01/Ex38(Funcao).cpp
@@ -0,0 +1,45 @@
+void calculaPreco(){
+	int cod, quant = 0;
+	
+	cout << "Informe o codigo do pedido: ";
+	cin >> cod;
+	cout << "Informe a quantidade que deseja comprar: ";
+	cin >> quant;
+	
+	switch (cod){
+		case 100:
+			cout << "O preco total sera R$" << quant * 2.20;
+			break;
+		case 101:
+			cout << "O preco total sera R$" << quant * 3.30;
+			break;
+		case 102:
+			cout << "O preco total sera R$" << quant * 3.50;
+			break;
+		case 103:
+			cout << "O preco total sera R$" << quant * 5.20;
+			break;
+		case 104:
+			cout << "O preco total sera R$" << quant * 7.30;
+			break;
+		case 105:
+			cout << "O preco total sera R$" << quant * 2.00;
+			break;
+		default:
+			cout << "Codigo invalido!" << endl << endl;
+			calculaPreco();
+	}	
+}
+
+int main(){
+	cout << "Especificacao        Codigo      Preco" << endl;
+	cout << "Cachorro quente:      100        2,20" << endl;
+	cout << "Bauru Simples:        101        3,30" << endl;
+	cout << "Bauru com Ovo:        102        3,50" << endl;
+	cout << "Hamburger:            103        5,20" << endl;
+	cout << "Cheeseburger:         104        7,30" << endl;
+	cout << "Refrigerante          105        2,00" << endl << endl;
+	calculaPreco();
+	
+	return 0;
+}
