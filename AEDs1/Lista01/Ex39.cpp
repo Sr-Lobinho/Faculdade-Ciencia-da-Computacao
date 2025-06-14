bfe8691e6678abb7652706252d3149fc5b941854
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 15:54:37 -0300

    Create Ex39.cpp

---
+++ AEDs1/Lista01/Ex39.cpp
@@ -0,0 +1,27 @@
+void imprimeEstacao(int m){
+	if (m >= 3 and m < 6){
+		cout << "Outono!";
+	}
+	else if (m >= 6 and m < 9){
+		cout << "Inverno!";
+	}
+	else if (m >= 9 and m < 12){
+		cout << "Primavera!";
+	}
+	else if (m <= 12 and m >= 0){
+		cout << "Verao!";
+	}
+	else{
+		cout << "Mes invalido!";
+	}
+}
+
+int main(int argc, char** argv) {
+	int mes =0;
+	cout << "Informe o mes(numero): ";
+	cin >> mes;
+	
+	imprimeEstacao(mes);
+	
+	return 0;
+}
