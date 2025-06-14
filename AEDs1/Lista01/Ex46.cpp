a315ef67aa4be5609b281ffb5a22459fb33b17ce
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 15:59:56 -0300

    Create Ex46.cpp

---
+++ AEDs1/Lista01/Ex46.cpp
@@ -0,0 +1,29 @@
+float calculaPreco(float kw){
+	float total = 0;
+	if (kw < 100){
+		total = kw * 0.5;
+	}
+	else if (kw < 200){
+		total = kw * 0.75;
+	}
+	else if (kw < 250){
+		total = kw * 1.20;
+	}
+	else{
+		total = kw * 1.50;
+	}
+	total += total * 0.2;
+	return total;
+	
+}
+
+int main(int argc, char** argv) {
+	float kw = 0;
+	cout << "Informe a quantidade de kW usado: ";
+	cin >> kw;
+	
+	cout << "O preco total sera " << calculaPreco(kw);
+	
+	
+	return 0;
+}
