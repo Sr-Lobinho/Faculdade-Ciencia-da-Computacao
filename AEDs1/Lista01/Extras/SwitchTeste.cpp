4907a6bc8a3aebcbe81f4e1fa880790ef33f24b4
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 16:38:47 -0300

    Create SwitchTeste.cpp

---
+++ AEDs1/Lista01/Extras/SwitchTeste.cpp
@@ -0,0 +1,24 @@
+int main(int argc, char** argv) {
+	int opcao = 0;
+	cout << "0 - Calcular Fatorial" << endl << "1 - Calcular Primo" << endl << "2 - Calcular Dobro" << endl << "3 - Sair do Programa" << endl;
+	cin >> opcao;
+	
+	switch(opcao){
+		case 0:
+			cout << "Calculando Fatorial...";
+			break;
+		case 1:
+			cout << "Calculando Primo... ";
+			break;
+		case 2:
+			cout << "Calculando Dobro...";
+			break;
+		case 3:
+			cout << "Fechando o programa...";
+			break;
+		default:
+			cout << "Opcao invalida!";
+		
+	}	
+	return 0;
+}
