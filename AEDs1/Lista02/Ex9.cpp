927be1d48481f63c0d12269bdc35c13f5667a97c
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 17:30:17 -0300

    Create Ex9.cpp

---
+++ AEDs1/Lista02/Ex9.cpp
@@ -0,0 +1,10 @@
+int main(){
+	float n = 0.0;
+	do{
+		cout << "Informe a nota: ";
+		cin >> n;
+		if (!(n >= 0 and n <= 10)){
+			cout << "Nota invalida!" << endl;
+		}
+	}while(!(n >= 0 and n <= 10));
+}
