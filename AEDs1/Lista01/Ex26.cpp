41f4a072b0e1a515038fa50cd3f2a0ee1fe48751
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 15:44:32 -0300

    Create Ex26.cpp

---
+++ AEDs1/Lista01/Ex26.cpp
@@ -0,0 +1,28 @@
+int calculaHora(int a1, int an){
+	if (an > a1){
+		return an - a1;
+	}
+	else{
+		int Falta = 24 - a1 + an;
+		return Falta;
+	}
+	
+}
+
+int main(int argc, char** argv) {
+	int hI, hF, resultado = 0;
+	cout << "Informe a hora de inicio do jogo: ";
+	cin >> hI;
+	cout << "Informe a hora em que o jogo terminou: ";
+	cin >> hF;
+	
+	resultado = calculaHora(hI,hF);
+	if (resultado > 24 or resultado < 1){
+		cout << "O horario esta fora do minimo/maximo.";
+	}
+	else{
+		cout << "O jogo durou " << resultado << " horas";
+	}
+	
+	return 0;
+}
