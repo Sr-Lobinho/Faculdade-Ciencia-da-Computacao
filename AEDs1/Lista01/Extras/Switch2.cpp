7cd9daacef3c5ce0205f560b1a9342dda84d04e4
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-14 16:39:37 -0300

    Create Switch2.cpp

---
+++ AEDs1/Lista01/Extras/Switch2.cpp
@@ -0,0 +1,23 @@
+int main(int argc, char** argv) {
+	char opcao =' ';
+	cout << "Digite uma letra do alfabeto: ";
+	cin >> opcao;
+	switch(opcao){
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+			cout << "E uma Vogal";
+			break; 	
+		default:
+			if (opcao > 'a' and opcao < 'z'){
+				cout << "E uma Consoante";
+			}
+			else{
+				cout << "Opcao invalida!";
+			}
+			break;
+	}
+	return 0;
+}
