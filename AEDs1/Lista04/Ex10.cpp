80e05987972490c91bbdb1f5236b27a7d3f06f7a
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-23 14:56:13 -0300

    Create Ex10.cpp

---
+++ AEDs1/Lista04/Ex10.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void leRaiz(float *vet){
+	float n = 0;
+	for (int i = 0; i < 15; i++){
+		cout << "Informe um numero para a posicao " << i << ": ";
+		cin >> n;
+		vet[i] = sqrt(n);
+	}
+}
+
+void imprimeNum(float *vet){
+	cout << "Vetor = [";
+	for (int i = 0; i < 14; i++){
+		cout << vet[i] << ", ";
+	}
+	cout << vet[14] << "]" << endl;
+}
+
+int main(){
+	float *vet = new float[15];
+	leRaiz(vet);
+	imprimeNum(vet);
+	return 0;
+}
