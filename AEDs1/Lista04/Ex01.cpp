3ec02d57596eba866c20fffaf164f520dc9da45e
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-06-23 09:35:52 -0300

    Update Ex01.cpp

--- AEDs1/Lista04/Ex01.cpp
+++ AEDs1/Lista04/Ex01.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void imprimeInverso(){
 	int vet[5];
 	for (int i = 0; i < 5; i++){
-		cout << "Informe o numero "<< i << ": ";
+		cout << "Informe um numero ("<< i +1 << "): ";
 		cin >> vet[i];
 	}
 	
