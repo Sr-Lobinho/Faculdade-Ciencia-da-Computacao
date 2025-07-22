e3ec3fbf6dd67924633da0b0f82a05fe744d4e84
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-07-22 07:39:56 -0300

    Update Ex15.cpp

--- AEDs1/Lista05/Ex15.cpp
+++ AEDs1/Lista05/Ex15.cpp
@@ -36,27 +36,65 @@ struct Poligono{
 			vertices[i].preechePonto(i+1);
 		}
 	}
-	bool verificaConvexo(){
-		
+	float produtoVetorial(Ponto2D &A, Ponto2D &B, Ponto2D &C){
+		float produto;
+		produto = (B.x-A.x)* (C.y - B.y)-(B.y-A.y)*(C.x-B.x);
+		return produto;
 	}
+	
+	bool verificaConvexo(){
+		bool eNegativo = false;
+		bool ePositivo = false;
+		float cross;
+		for (int i = 0; i < tam; i ++){
+			if (i < tam-2){
+				cross = produtoVetorial(vertices[i],vertices[i+1],vertices[i+2]);
+			}
+			else if(i == tam-2){
+				cross = produtoVetorial(vertices[tam-2],vertices[tam-1],vertices[0]);
+			}
+			else if (i == tam-1){
+				cross = produtoVetorial(vertices[tam-1],vertices[0],vertices[1]);
+			}
+			if (cross < 0){
+				eNegativo = true;
+			}
+			else if (cross > 0){
+				ePositivo = true;
+			}
+			
+		}
+		if (eNegativo and ePositivo){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
 	// Area = 1/2 *[(x1y2 + x2y3 ... xny1)- (y1x2 +y2x3 ... ynx1)]
 	void calculaArea(){
 		float a1 = 0, a2 = 0;
-		for (int i = 0; i < tam; i++){
-			if (i == tam - 1){
-				a1 += (vertices[i].x * vertices[0].y);
-				a2 += (vertices[i].y * vertices[0].x);
+		if (verificaConvexo()){
+			for (int i = 0; i < tam; i++){
+				if (i == tam - 1){
+					a1 += (vertices[i].x * vertices[0].y);
+					a2 += (vertices[i].y * vertices[0].x);
+				}
+				else{
+					a1 += (vertices[i].x * vertices[i+1].y);
+					a2 += (vertices[i].y * vertices[i+1].x);
+				}
 			}
-			else{
-				a1 += (vertices[i].x * vertices[i+1].y);
-				a2 += (vertices[i].y * vertices[i+1].x);
+			area = 0.5 *(a1 - a2);
+			if (area < 0){
+				area *= -1;
 			}
+			cout << "Area: " << area;
 		}
-		area = 0.5 *(a1 - a2);
-		if (area < 0){
-			area *= -1;
+		else{
+			cout << "Poligono nao convexo!" << endl;
 		}
-		cout << "Area: " << area;
 	}
 	
 	
