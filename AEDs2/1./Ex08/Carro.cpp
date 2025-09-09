1e9bca169f6b27d0e101d655a57aec9a7b42321a
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-09-09 16:44:54 -0300

    Update Carro.cpp

--- AEDs2/1./Ex08/Carro.cpp
+++ AEDs2/1./Ex08/Carro.cpp
@@ -18,6 +18,27 @@ Carro::Carro(const Carro & outro){
     this->valor = outro.getValor();
 }
 
+void Carro::imprimir(){
+    cout << "Nome: " << this->nome << endl;
+    cout << "Marca: " << this->marca << endl;
+    cout << "Motorizacao: " << this->motorizacao << endl;
+    cout << "Cor: " << this->cor << endl;
+    cout << "Valor: " << this->valor << endl;
+}
+
+void Carro::preencher(){
+    cout << "Informe o nome: ";
+    cin >> this->nome;
+    cout << "Informe a marca: ";
+    cin >> this->marca;
+    cout << "Informe a motorizacao: ";
+    cin >> this->motorizacao;
+    cout << "Informe a cor: ";
+    cin >> this->cor;
+    cout << "Informe o valor: ";
+    cin >> this->valor;
+}
+
 
 void Carro::setNome(string nome){
     this->nome = nome;
