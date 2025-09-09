f20b927206d662634b50e00257212d69a51d9462
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-09-09 16:25:47 -0300

    Update Carro.h

--- AEDs2/1./Ex08/Carro.h
+++ AEDs2/1./Ex08/Carro.h
@@ -3,6 +3,29 @@
 using namespace std;
 
 class Carro{
+    private:
+        string nome;
+        string marca;
+        string motorizacao;
+        string cor;
+        float valor;
+    public:
+        Carro();
+        Carro(Carro & outro);
+
+        void imprimir();
+        void preencher();
+
+        void setNome(string nome);
+        string getNome();
+        void setMarca(string marca);
+        string getMarca();
+        void setMotorizacao(string motorizacao);
+        string getMotorizacao();
+        void setCor(string cor);
+        string getCor();
+        void setValor(float valor);
+        float getValor();
 
 
 };
