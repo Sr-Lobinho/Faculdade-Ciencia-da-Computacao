bbd2da5de5172ac88de37b8a2dced06788af3d9c
Sr-Lobinho <168032801+Sr-Lobinho@users.noreply.github.com>
2025-09-09 16:32:06 -0300

    Update Carro.cpp

--- AEDs2/1./Ex08/Carro.cpp
+++ AEDs2/1./Ex08/Carro.cpp
@@ -2,3 +2,19 @@
 #include <iostream>
 using namespace std;
 
+Carro::Carro(){
+    nome = " ";
+    marca = " ";
+    motorizacao = " ";
+    cor = " ";
+    valor = 0.0;
+}
+
+Carro::Carro(const Carro & outro){
+    this->nome = outro.getNome();
+    this->marca = outro.getMarca();
+    this->motorizacao = outro.getMotorizacao();
+    this->cor = outro.getCor();
+    this->valor = outro.getValor();
+}
+
